c91107457ea8d9543fa7c389d51f239833036eab
vamsi <bellamvamsikrishna3415@gmail.com>
2021-04-22 20:25:34 +0530

    justjs should be separate

---
+++ dst/tuples.js
@@ -0,0 +1,7 @@
+// an Array kind of thing that keeps mixed data types.
+var quest = function (pair) {
+    var name = pair[0];
+    var age = pair[1];
+    console.log("Hey, Hello " + name + " and you are " + age + " years old right ?");
+};
+quest(["Ajay", 20]);
